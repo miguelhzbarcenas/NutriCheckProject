651e978bc33f2158e725fd204eb65b69847f336f
miguelbarcenas <miguelhz.bar@gmail.com>
2025-10-04 02:44:03 -0600

    Initial Commit

---
+++ NutriCheckProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriCheckProject
+//
+//  Created by Miguel Angel Hernandez Barcenas on 04/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
